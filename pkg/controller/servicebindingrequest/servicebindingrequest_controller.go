78b14b763dd381fe3424baaa115a34340e49003d
Baiju Muthukadan <baiju.m.mail@gmail.com>
2019-06-21 07:09:27 +0530

    Remove comment

--- pkg/controller/servicebindingrequest/servicebindingrequest_controller.go
+++ pkg/controller/servicebindingrequest/servicebindingrequest_controller.go
@@ -25,11 +25,6 @@ import (
 
 var log = logf.Log.WithName("controller_servicebindingrequest")
 
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
-
 // Add creates a new ServiceBindingRequest Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
