52122584313ef8c2c4662a08f7930d9600874cf9
Otávio Fernandes <otaviof@gmail.com>
2019-06-27 10:15:47 +0200

    Able to hook required CRDs.

--- test/e2e/servicebindingrequest_test.go
+++ test/e2e/servicebindingrequest_test.go
@@ -2,12 +2,15 @@ package e2e
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 	"time"
 
 	olmv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
+	olminstall "github.com/operator-framework/operator-lifecycle-manager/pkg/controller/install"
 	framework "github.com/operator-framework/operator-sdk/pkg/test"
 	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -24,31 +27,32 @@ var (
 	replicas             = 1
 	operatorKind         = "ServiceBindingRequest"
 	operatorAPIVersion   = "apps.openshift.io/v1alpha1"
+	olmKind              = "CustomResourceDefinition"
+	olmAPIVersion        = "apiextensions.k8s.io/v1beta1"
+	crdName              = "e2e-resource-name"
+	crdVersion           = "0.0.1"
+	secretName           = "e2e-secret"
 )
 
-// TestAddToScheme main method for end-to-end testing, will call out the other components in order
-// to test service-binding-operator, after including custom-resource-definitions are part of the
-// local scheme.
-func TestAddToScheme(t *testing.T) {
-	var err error
-
-	t.Log("Adding ServiceBindingRequest scheme to cluster...")
+func TestAddSchemesToFramework(t *testing.T) {
 	serviceBindingRequestList := &v1alpha1.ServiceBindingRequestList{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       operatorKind,
-			APIVersion: operatorAPIVersion,
-		},
+		Items: []v1alpha1.ServiceBindingRequest{v1alpha1.ServiceBindingRequest{}},
 	}
 
-	if err = framework.AddToFrameworkScheme(apis.AddToScheme, serviceBindingRequestList); err != nil {
-		t.Fatalf("Error on adding ServiceBindingRequest CRD to cluster!")
+	t.Log("Adding ServiceBindingRequest scheme to cluster...")
+	err := framework.AddToFrameworkScheme(apis.AddToScheme, serviceBindingRequestList)
+	if err != nil {
+		t.Fatalf("Error on adding ServiceBindingRequest CRD: '%s'", err)
+	}
+
+	clusterServiceVersionListObj := &olmv1alpha1.ClusterServiceVersionList{
+		Items: []olmv1alpha1.ClusterServiceVersion{olmv1alpha1.ClusterServiceVersion{}},
 	}
 
 	t.Log("Adding ClusterServiceVersion scheme to cluster...")
-	clusterServiceVersionListObj := &olmv1alpha1.ClusterServiceVersionList{}
-
-	if err = framework.AddToFrameworkScheme(apis.AddToScheme, clusterServiceVersionListObj); err != nil {
-		t.Fatalf("Error on adding ServiceBindingRequest CRD to cluster!")
+	err = framework.AddToFrameworkScheme(olmv1alpha1.AddToScheme, clusterServiceVersionListObj)
+	if err != nil {
+		t.Fatalf("Error on adding ClusterServiceVersion CRD: '%s'", err)
 	}
 
 	t.Run("end-to-end", func(t *testing.T) {
@@ -61,91 +65,6 @@ func cleanUpOptions(ctx *framework.TestCtx) *framework.CleanupOptions {
 		TestContext:   ctx,
 		Timeout:       cleanupTimeout,
 		RetryInterval: time.Duration(time.Second * retryInterval),
-	}
-}
-
-func mockedObjects(t *testing.T, ns string, f *framework.Framework, ctx *framework.TestCtx) {
-	var err error
-
-	t.Log("Starting end-to-end tests for operator...")
-
-	crdName := "e2e-resource-name"
-	crdVersion := "0.0.1"
-	secretName := "e2e-secret"
-
-	/*
-		clusterServiceVersionObj := olmv1alpha1.ClusterServiceVersion{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "ClusterServiceVersion",
-				APIVersion: "operators.coreos.com/v1alpha1",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "e2e-cluster-service-version",
-				Namespace: ns,
-			},
-			Spec: olmv1alpha1.ClusterServiceVersionSpec{
-				CustomResourceDefinitions: olmv1alpha1.CustomResourceDefinitions{
-					Owned: []olmv1alpha1.CRDDescription{{
-						Name:    crdName,
-						Version: crdVersion,
-						SpecDescriptors: []olmv1alpha1.SpecDescriptor{{
-							DisplayName:  secretName,
-							Path:         secretName,
-							XDescriptors: []string{"urn:alm:descriptor:io.kubernetes:Secret"},
-						}},
-					}},
-				},
-			},
-		}
-
-		clusterServiceVersionListObj := olmv1alpha1.ClusterServiceVersionList{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "ClusterServiceVersionList",
-				APIVersion: "operators.coreos.com/v1alpha1",
-			},
-			Items: []olmv1alpha1.ClusterServiceVersion{clusterServiceVersionObj},
-		}
-
-		if err = f.Client.Create(context.TODO(), &clusterServiceVersionListObj, cleanUpOptions(ctx)); err != nil {
-			t.Fatalf("Error on creating CSV list object: '%s'", err)
-		}
-	*/
-
-	secretObj := corev1.Secret{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Secret",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      secretName,
-			Namespace: ns,
-		},
-		Data: map[string][]byte{"secret-entry": []byte("secret-value")},
-	}
-
-	if err = f.Client.Create(context.TODO(), &secretObj, cleanUpOptions(ctx)); err != nil {
-		t.Fatalf("Error on creating secret object: '%s'", err)
-	}
-
-	serviceBindingRequestObj := v1alpha1.ServiceBindingRequest{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       operatorKind,
-			APIVersion: operatorAPIVersion,
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "e2e-bind-request",
-			Namespace: ns,
-		},
-		Spec: v1alpha1.ServiceBindingRequestSpec{
-			BackingSelector: v1alpha1.BackingSelector{
-				ResourceName:    crdName,
-				ResourceVersion: crdVersion,
-			},
-		},
-	}
-
-	if err = f.Client.Create(context.TODO(), &serviceBindingRequestObj, cleanUpOptions(ctx)); err != nil {
-		t.Fatalf("Error on creating service-binding-request object: '%s'", err)
 	}
 }
 
@@ -186,6 +105,106 @@ func ServiceBindingRequest(t *testing.T) {
 	serviceBindingRequestTest(t, ns, f, ctx)
 }
 
+func mockedObjects(t *testing.T, ns string, f *framework.Framework, ctx *framework.TestCtx) {
+	t.Log("Starting end-to-end tests for operator...")
+
+	strategy := olminstall.StrategyDetailsDeployment{
+		DeploymentSpecs: []olminstall.StrategyDeploymentSpec{{
+			Name: "deployment",
+			Spec: appsv1.DeploymentSpec{},
+		}},
+	}
+
+	strategyJSON, err := json.Marshal(strategy)
+	if err != nil {
+		t.Fatalf("Error on encoding install strategy to JSON: '%s'", err)
+	}
+
+	clusterServiceVersionObj := olmv1alpha1.ClusterServiceVersion{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ClusterServiceVersion",
+			APIVersion: "operators.coreos.com/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "e2e-cluster-service-version",
+			Namespace: ns,
+		},
+		Spec: olmv1alpha1.ClusterServiceVersionSpec{
+			DisplayName: "e2e csv",
+			InstallStrategy: olmv1alpha1.NamedInstallStrategy{
+				StrategyName:    "deployment",
+				StrategySpecRaw: strategyJSON,
+			},
+			CustomResourceDefinitions: olmv1alpha1.CustomResourceDefinitions{
+				Owned: []olmv1alpha1.CRDDescription{{
+					DisplayName: crdName,
+					Name:        crdName,
+					Version:     crdVersion,
+					Description: "e2e csv example",
+					SpecDescriptors: []olmv1alpha1.SpecDescriptor{{
+						DisplayName:  secretName,
+						Description:  "e2e csv example secret",
+						Path:         secretName,
+						XDescriptors: []string{"urn:alm:descriptor:io.kubernetes:Secret"},
+					}},
+				}},
+			},
+		},
+	}
+
+	t.Log("Creating ClusterServiceVersion object...")
+	err = f.Client.Create(context.TODO(), &clusterServiceVersionObj, cleanUpOptions(ctx))
+	if err != nil {
+		t.Fatalf("Error on creating ClusterServiceVersion object: '%s'", err)
+	}
+
+	secretObj := corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      secretName,
+			Namespace: ns,
+		},
+		Data: map[string][]byte{"secret-entry": []byte("secret-value")},
+	}
+
+	t.Log("Creating secret object...")
+	err = f.Client.Create(context.TODO(), &secretObj, cleanUpOptions(ctx))
+	if err != nil {
+		t.Fatalf("Error on creating secret object: '%s'", err)
+	}
+
+	serviceBindingRequestObj := v1alpha1.ServiceBindingRequest{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       operatorKind,
+			APIVersion: operatorAPIVersion,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "e2e-bind-request",
+			Namespace: ns,
+		},
+		Spec: v1alpha1.ServiceBindingRequestSpec{
+			BackingSelector: v1alpha1.BackingSelector{
+				ResourceName:    crdName,
+				ResourceVersion: crdVersion,
+			},
+			ApplicationSelector: v1alpha1.ApplicationSelector{
+				MatchLabels: map[string]string{
+					"connects-to": "postgres",
+					"environment": "production",
+				},
+			},
+		},
+	}
+
+	t.Log("Creating ServiceBindingRequest object...")
+	err = f.Client.Create(context.TODO(), &serviceBindingRequestObj, cleanUpOptions(ctx))
+	if err != nil {
+		t.Fatalf("Error on creating service-binding-request object: '%s'", err)
+	}
+}
 func serviceBindingRequestTest(t *testing.T, ns string, f *framework.Framework, ctx *framework.TestCtx) {
 	mockedObjects(t, ns, f, ctx)
 }
