b715b8df5b0316729902b887a36e3a5091600c94
Dhriti Shikhar <dhriti.shikhar.rokz@gmail.com>
2019-07-01 11:00:18 +0530

    Add Makefile target for CI (#35)

---
+++ hack/check-crds.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function check_crds() {
+	local crd_name=$1
+	for i in {1..120}
+	do
+		kubectl get crds | grep $crd_name
+		if [ $? == 0 ]
+		then
+			echo "CRD exists: " $crd_name
+			return 0
+		fi
+		sleep 10s
+	done
+	echo "CRD doesn't exist: " $crd_name
+	exit 1
+}
+
+check_crds servicebindingrequests.apps.openshift.io
