308736890fc75313bc4df009adf637875fc057b4
Shoubhik Bose <shbose@redhat.com>
2019-08-11 08:22:45 -0400

    attach predicate

--- pkg/controller/servicebindingrequest/controller.go
+++ pkg/controller/servicebindingrequest/controller.go
@@ -43,7 +43,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	}
 
 	// Watch for changes to primary resource ServiceBindingRequest
-	err = c.Watch(&source.Kind{Type: &v1alpha1.ServiceBindingRequest{}}, &handler.EnqueueRequestForObject{})
+	err = c.Watch(&source.Kind{Type: &v1alpha1.ServiceBindingRequest{}}, &handler.EnqueueRequestForObject{},pred)
 	if err != nil {
 		return err
 	}
