ecbf50e0f8d1fadb1959d0c8c450975b60e9a601
Otávio Fernandes <otaviof@gmail.com>
2019-08-18 10:54:11 +0200

    Refactoring tests to use "fake".

--- pkg/controller/servicebindingrequest/binder_test.go
+++ pkg/controller/servicebindingrequest/binder_test.go
@@ -6,57 +6,51 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/dynamic"
-	fakedynamic "k8s.io/client-go/dynamic/fake"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 
-	v1alpha1 "github.com/redhat-developer/service-binding-operator/pkg/apis/apps/v1alpha1"
 	"github.com/redhat-developer/service-binding-operator/test/mocks"
 )
 
-var binder *Binder
 var binderFakeClient client.Client
 var binderFakeDynClient dynamic.Interface
 
-func TestBinder(t *testing.T) {
+func init() {
 	logf.SetLogger(logf.ZapLogger(true))
+}
 
+func TestBinderNew(t *testing.T) {
 	ns := "binder"
 	name := "service-binding-request"
-
-	s := runtime.NewScheme()
 	matchLabels := map[string]string{
 		"connects-to": "database",
 		"environment": "binder",
 	}
 
-	sbr := mocks.ServiceBindingRequestMock(ns, name, name, matchLabels)
-	s.AddKnownTypes(v1alpha1.SchemeGroupVersion, &v1alpha1.ServiceBindingRequest{})
+	f := mocks.NewFake(t, ns)
+	sbr := f.AddMockedServiceBindingRequest(name, "ref", matchLabels)
+	f.AddMockedUnstructuredDeployment("ref", matchLabels)
 
-	require.Nil(t, appsv1.AddToScheme(s))
-	d, err := mocks.UnstructuredDeploymentMock(ns, name, matchLabels)
-	require.Nil(t, err)
-
-	objs := []runtime.Object{sbr, d}
-	binderFakeClient = fake.NewFakeClientWithScheme(s, objs...)
-	binderFakeDynClient = fakedynamic.NewSimpleDynamicClient(s, objs...)
-	binder = NewBinder(context.TODO(), binderFakeClient, binderFakeDynClient, sbr, []string{})
+	binder := NewBinder(
+		context.TODO(),
+		f.FakeClient(),
+		f.FakeDynClient(),
+		sbr,
+		[]string{},
+	)
 
 	require.NotNil(t, binder)
-}
 
-func TestBinderSearch(t *testing.T) {
-	list, err := binder.search()
-
-	assert.Nil(t, err)
-	assert.Equal(t, 1, len(list.Items))
+	t.Run("search", func(t *testing.T) {
+		list, err := binder.search()
+		assert.Nil(t, err)
+		assert.Equal(t, 1, len(list.Items))
+	})
 }
 
 func TestBinderAppendEnvFrom(t *testing.T) {
+	binder := &Binder{}
 	secretName := "secret"
 	d := mocks.DeploymentMock("binder", "binder", map[string]string{})
 	list := binder.appendEnvFrom(d.Spec.Template.Spec.Containers[0].EnvFrom, secretName)
--- pkg/controller/servicebindingrequest/planner_test.go
+++ pkg/controller/servicebindingrequest/planner_test.go
@@ -4,72 +4,58 @@ import (
 	"context"
 	"testing"
 
-	pgapis "github.com/baijum/postgresql-operator/pkg/apis"
-	pgv1alpha1 "github.com/baijum/postgresql-operator/pkg/apis/postgresql/v1alpha1"
-	olmv1alpha1 "github.com/operator-framework/operator-lifecycle-manager/pkg/api/apis/operators/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes/scheme"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 
-	"github.com/redhat-developer/service-binding-operator/pkg/apis/apps/v1alpha1"
 	"github.com/redhat-developer/service-binding-operator/test/mocks"
 )
 
 var planner *Planner
 
-func TestPlannerNew(t *testing.T) {
+func init() {
 	logf.SetLogger(logf.ZapLogger(true))
+}
 
-	ns := "testing"
+func TestPlannerNew(t *testing.T) {
+	ns := "planner"
+	name := "service-binding-request"
 	resourceRef := "db-testing"
-	s := scheme.Scheme
-
-	sbr := mocks.ServiceBindingRequestMock(ns, "binding-request", resourceRef, map[string]string{
+	matchLabels := map[string]string{
 		"connects-to": "database",
 		"environment": "planner",
-	})
-	s.AddKnownTypes(v1alpha1.SchemeGroupVersion, sbr)
+	}
 
-	require.Nil(t, olmv1alpha1.AddToScheme(s))
-	csvList := mocks.ClusterServiceVersionListMock(ns, "cluster-service-version-list")
-	s.AddKnownTypes(olmv1alpha1.SchemeGroupVersion, csvList)
+	f := mocks.NewFake(t, ns)
+	sbr := f.AddMockedServiceBindingRequest(name, resourceRef, matchLabels)
+	f.AddMockedCSVList("cluster-service-version-list")
+	f.AddMockedDatabaseCRList(resourceRef)
 
-	require.Nil(t, pgapis.AddToScheme(s))
-	crList := mocks.DatabaseCRListMock(ns, resourceRef)
-	s.AddKnownTypes(pgv1alpha1.SchemeGroupVersion, crList)
-
-	objs := []runtime.Object{sbr, csvList, crList}
-	fakeClient := fake.NewFakeClientWithScheme(s, objs...)
-
-	planner = NewPlanner(context.TODO(), fakeClient, ns, sbr)
+	planner = NewPlanner(context.TODO(), f.FakeClient(), ns, sbr)
 	require.NotNil(t, planner)
-}
 
-func TestPlannerSearchCRDDescription(t *testing.T) {
-	TestPlannerNew(t)
-	crdDescription, err := planner.searchCRDDescription()
+	t.Run("searchCRDDescription", func(t *testing.T) {
+		crdDescription, err := planner.searchCRDDescription()
 
-	assert.Nil(t, err)
-	assert.NotNil(t, crdDescription)
-}
+		assert.Nil(t, err)
+		assert.NotNil(t, crdDescription)
+	})
 
-func TestPlannerSearchCR(t *testing.T) {
-	cr, err := planner.searchCR("Database")
+	t.Run("searchCR", func(t *testing.T) {
+		cr, err := planner.searchCR("Database")
 
-	assert.Nil(t, err)
-	assert.NotNil(t, cr)
-}
+		assert.Nil(t, err)
+		assert.NotNil(t, cr)
+	})
 
-func TestPlannerPlan(t *testing.T) {
-	plan, err := planner.Plan()
+	t.Run("plan", func(t *testing.T) {
+		plan, err := planner.Plan()
 
-	assert.Nil(t, err)
-	assert.NotNil(t, plan)
-	assert.NotNil(t, plan.CRDDescription)
-	assert.NotNil(t, plan.CR)
-	assert.Equal(t, "testing", plan.Ns)
-	assert.Equal(t, "binding-request", plan.Name)
+		assert.Nil(t, err)
+		assert.NotNil(t, plan)
+		assert.NotNil(t, plan.CRDDescription)
+		assert.NotNil(t, plan.CR)
+		assert.Equal(t, ns, plan.Ns)
+		assert.Equal(t, name, plan.Name)
+	})
 }
