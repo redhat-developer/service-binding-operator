fce92e38cf97fdd8b72638ef0229d32a46ca58fb
Otávio Fernandes <otaviof@gmail.com>
2019-08-25 07:42:15 +0200

    Trigger Rebinding
    Adding back snippet that will change on the fly the spec of a SBR.
    This feature should be re-discussed in the light of `metadata.generation`
    field.

--- pkg/controller/servicebindingrequest/controller.go
+++ pkg/controller/servicebindingrequest/controller.go
@@ -48,6 +48,21 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 
 	pred := predicate.Funcs{
 		UpdateFunc: func(e event.UpdateEvent) bool {
+			oldKind := e.ObjectOld.DeepCopyObject().GetObjectKind().GroupVersionKind().Kind
+			if oldKind == ServiceBindingRequestKind {
+				oldSBR := e.ObjectOld.(*v1alpha1.ServiceBindingRequest)
+				newSBR := e.ObjectNew.(*v1alpha1.ServiceBindingRequest)
+
+				// if event was triggered as part of resetting TriggerRebinding True to False,
+				// we shall ignore it to avoid an infinite loop.
+				if newSBR.Spec.TriggerRebinding != nil &&
+					oldSBR.Spec.TriggerRebinding != nil &&
+					!*newSBR.Spec.TriggerRebinding &&
+					*oldSBR.Spec.TriggerRebinding {
+					return false
+				}
+			}
+
 			// ignore updates to CR status in which case metadata.Generation does not change
 			return e.MetaOld.GetGeneration() != e.MetaNew.GetGeneration()
 		},
