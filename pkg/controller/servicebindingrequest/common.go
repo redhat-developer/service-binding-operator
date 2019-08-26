0df82f3e78fe6b5237af08ed6d1973b22370a772
Otávio Fernandes <otaviof@gmail.com>
2019-08-26 23:06:23 +0200

    Adds DeploymentConfig kind constant.

--- pkg/controller/servicebindingrequest/common.go
+++ pkg/controller/servicebindingrequest/common.go
@@ -15,6 +15,9 @@ var (
 // ServiceBindingRequestKind defines the name of the CRD Kind.
 const ServiceBindingRequestKind = "ServiceBindingRequest"
 
+// DeploymentConfigKind defines the name of DeploymentConfig kind.
+const DeploymentConfigKind = "DeploymentConfig"
+
 // RequeueOnNotFound inspect error, if not-found then returns Requeue, otherwise expose the error.
 func RequeueOnNotFound(err error, requeueAfter int64) (reconcile.Result, error) {
 	if errors.IsNotFound(err) {
