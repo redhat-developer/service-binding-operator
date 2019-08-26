d9b6b4adaf00c0b6b1cd14320e32dec2a29773ba
Shoubhik Bose <shbose@redhat.com>
2019-08-26 17:21:31 -0400

    code to an interface

--- pkg/controller/servicebindingrequest/collectors/cr.go
+++ pkg/controller/servicebindingrequest/collectors/cr.go
@@ -1,1 +1,22 @@
 package collectors
+
+// CrCollector reads all data referred in plan instance
+type CrCollector struct {
+	ctx           context.Context // request context
+	client        client.Client   // Kubernetes API client
+}
+
+// NewCSVCollector creates a new CSVCollector
+func NewCrCollector(ctx context.Context, client client.Client) CrCollector {
+	return CrCollector{
+		ctx:           ctx,
+		client:        client,
+}
+
+// Collect returns all bindable metadata
+func (c *CrCollector) Collect() (*BindableMetadata, error) {
+	/*
+		Read from the CR JSON path
+	*/
+	return nil, nil
+}
