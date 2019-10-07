1590d7489e44d4634023aa57883cfb1cf2ea1bf9
Baiju Muthukadan <baiju.m.mail@gmail.com>
2019-10-07 11:59:48 +0530

    Add deps

--- pkg/controller/servicebindingrequest/planner_test.go
+++ pkg/controller/servicebindingrequest/planner_test.go
@@ -30,6 +30,8 @@ func TestPlannerNew(t *testing.T) {
 	sbr := f.AddMockedServiceBindingRequest(name, resourceRef, "", matchLabels)
 	f.AddMockedUnstructuredCSV("cluster-service-version")
 	f.AddMockedDatabaseCR(resourceRef)
+	f.AddMockedUnstructuredDatabaseCRD()
+	f.AddMockedUnstructuredPostgresDatabaseCR("dbtesting2")
 
 	planner = NewPlanner(context.TODO(), f.FakeDynClient(), sbr)
 	require.NotNil(t, planner)
