bc8ba8b9ff04e5fb943072a52487ca310c0383bc
Avni Sharma <16avnisharma@gmail.com>
2019-10-30 16:19:57 +0530

    Changes introduced to update in manifests-upstream/version/

--- hack/update-version.sh
+++ hack/update-version.sh
@@ -5,16 +5,10 @@
 set -e
 set -u
 
-MANIFESTS_DIR="./../manifests"
+MANIFESTS_DIR="./../manifests-upstream"
 NEW_VERSION=$1
-
-function current_version {
-    filename="../Makefile"
-    OPERATOR_VERSION=$(grep -m 1 OPERATOR_VERSION $filename | sed 's/^.*= //g')
-}
-
-current_version
-OLD_VERSION="${OPERATOR_VERSION}"
+filename="../Makefile"
+OLD_VERSION=$(grep -m 1 OPERATOR_VERSION $filename | sed 's/^.*= //g')
 
 function replace {
     LOCATION=$1
@@ -25,12 +19,22 @@ function replace {
         exit 1 #terminate and indicate error
     fi
 }
-replace ../Makefile
-replace ./update-version.sh
+function move {
+    OLD_LOCATION=$1
+    NEW_LOCATION=$2
+    if [ -e ${OLD_LOCATION} ] || [ -e ${NEW_LOCATION} ] ; then
+        mv ${OLD_LOCATION} ${NEW_LOCATION}
+    else
+        echo ERROR: Failed to find file location
+        exit 1 #terminate and indicate error
+    fi
+}
+move ${MANIFESTS_DIR}/${OLD_VERSION} ${MANIFESTS_DIR}/${NEW_VERSION}
 replace ${MANIFESTS_DIR}/service-binding-operator.package.yaml
-replace ${MANIFESTS_DIR}/service-binding-operator.v${OLD_VERSION}.clusterserviceversion.yaml
-mv ${MANIFESTS_DIR}/service-binding-operator.v${OLD_VERSION}.clusterserviceversion.yaml \
-${MANIFESTS_DIR}/service-binding-operator.v${NEW_VERSION}.clusterserviceversion.yaml
+move ${MANIFESTS_DIR}/${NEW_VERSION}/service-binding-operator.v${OLD_VERSION}.clusterserviceversion.yaml \
+${MANIFESTS_DIR}/${NEW_VERSION}/service-binding-operator.v${NEW_VERSION}.clusterserviceversion.yaml
+replace ${MANIFESTS_DIR}/${NEW_VERSION}/service-binding-operator.v${NEW_VERSION}.clusterserviceversion.yaml
 replace ./../openshift-ci/Dockerfile.registry.build
+replace ${filename}
 echo -e "\n\033[0;32m \xE2\x9C\x94 Operator version upgraded from \
 ${OLD_VERSION} to ${NEW_VERSION} \033[0m\n"
