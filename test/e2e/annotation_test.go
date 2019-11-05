108227a1a6a7608045525a6d5677fea1078e868d
Pavel Macík <pavel.macik@gmail.com>
2019-11-05 09:35:53 +0100

    Unify test asserts using require package.

--- test/e2e/annotation_test.go
+++ test/e2e/annotation_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/redhat-developer/service-binding-operator/pkg/apis"
 	"github.com/redhat-developer/service-binding-operator/pkg/apis/apps/v1alpha1"
 	"github.com/redhat-developer/service-binding-operator/test/mocks"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -21,33 +20,28 @@ import (
 
 func TestAnnoationBasedMetadata(t *testing.T) {
 	sbrlist := v1alpha1.ServiceBindingRequestList{}
-	err := framework.AddToFrameworkScheme(apis.AddToScheme, &sbrlist)
-	if err != nil {
-		t.Fatalf("failed to add custom resource scheme to framework: %v", err)
-	}
+	require.NoError(t, framework.AddToFrameworkScheme(apis.AddToScheme, &sbrlist), "failed to add custom resource scheme to framework")
 
 	dbList := pgv1alpha1.DatabaseList{}
-	require.Nil(t, framework.AddToFrameworkScheme(pgsqlapis.AddToScheme, &dbList))
+	require.NoError(t, framework.AddToFrameworkScheme(pgsqlapis.AddToScheme, &dbList))
 
 	dpList := appsv1.DeploymentList{}
-	require.Nil(t, framework.AddToFrameworkScheme(appsv1.AddToScheme, &dpList))
+	require.NoError(t, framework.AddToFrameworkScheme(appsv1.AddToScheme, &dpList))
 
 	secList := corev1.SecretList{}
-	require.Nil(t, framework.AddToFrameworkScheme(corev1.AddToScheme, &secList))
+	require.NoError(t, framework.AddToFrameworkScheme(corev1.AddToScheme, &secList))
 
 	ctx := framework.NewTestCtx(t)
 	defer ctx.Cleanup()
 
-	err = ctx.InitializeClusterResources(&framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: retryInterval})
-	if err != nil {
-		t.Fatalf("failed to initialize cluster resources: %v", err)
-	}
+	err := ctx.InitializeClusterResources(
+		&framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: retryInterval},
+	)
+	require.NoError(t, err, "failed to initialize cluster resources")
 
 	// get namespace
 	namespace, err := ctx.GetNamespace()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	// get global framework variables
 	f := framework.Global
@@ -56,9 +50,7 @@ func TestAnnoationBasedMetadata(t *testing.T) {
 	db := mocks.DatabaseCRMock(namespace, resourceRef)
 
 	err = f.Client.Create(context.TODO(), db, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	// create service binding request custom resource
 	name := "e2e-service-binding-request"
@@ -69,35 +61,25 @@ func TestAnnoationBasedMetadata(t *testing.T) {
 	appResourceRef := "example1"
 	dp := mocks.DeploymentMock(namespace, appResourceRef, matchLabels)
 	err = f.Client.Create(context.TODO(), &dp, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	sbr := mocks.ServiceBindingRequestMock(namespace, name, resourceRef, appResourceRef, matchLabels, false)
 	err = f.Client.Create(context.TODO(), sbr, &framework.CleanupOptions{TestContext: ctx, Timeout: time.Second * 5, RetryInterval: time.Second * 1})
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	time.Sleep(1 * time.Minute)
 
 	namespacedName := types.NamespacedName{Namespace: namespace, Name: name}
 	sbr2 := &v1alpha1.ServiceBindingRequest{}
 
-	if err = f.Client.Get(context.TODO(), namespacedName, sbr2); err != nil {
-		t.Error("SBR Not found", err)
-	}
+	require.NoError(t, f.Client.Get(context.TODO(), namespacedName, sbr2), "SBR Not found")
 
 	sbrSecret := &corev1.Secret{}
-	if err = f.Client.Get(context.TODO(), namespacedName, sbrSecret); err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, []byte("test-db"), sbrSecret.Data["DATABASE_DBNAME"], "Name not equal")
+	require.NoError(t, f.Client.Get(context.TODO(), namespacedName, sbrSecret))
+	require.Equal(t, []byte("test-db"), sbrSecret.Data["DATABASE_DBNAME"], "Name not equal")
 
 	dep := &appsv1.Deployment{}
 	namespacedName2 := types.NamespacedName{Namespace: namespace, Name: appResourceRef}
-	if err = f.Client.Get(context.TODO(), namespacedName2, dep); err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, name, dep.Spec.Template.Spec.Containers[0].EnvFrom[0].SecretRef.LocalObjectReference.Name, "secret reference doesn't match")
+	require.NoError(t, f.Client.Get(context.TODO(), namespacedName2, dep))
+	require.Equal(t, name, dep.Spec.Template.Spec.Containers[0].EnvFrom[0].SecretRef.LocalObjectReference.Name, "secret reference doesn't match")
 }
