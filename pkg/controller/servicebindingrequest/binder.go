3eea0e5e501ac0e57c95be1e31ecc46d26f3d7b2
Igor Sutton <igorsutton@me.com>
2019-11-15 15:53:43 +0100

    Comment variable change temporarily
    Since this changed every reconciliation operation, the application's
    pod was always being re-deployed.

--- pkg/controller/servicebindingrequest/binder.go
+++ pkg/controller/servicebindingrequest/binder.go
@@ -245,7 +245,7 @@ func (b *Binder) updateContainer(container interface{}) (map[string]interface{},
 	}
 	// effectively binding the application with intermediary secret
 	c.EnvFrom = b.appendEnvFrom(c.EnvFrom, b.sbr.GetName())
-	c.Env = b.appendEnvVar(c.Env, lastboundparam, time.Now().Format(time.RFC3339))
+	// c.Env = b.appendEnvVar(c.Env, lastboundparam, time.Now().Format(time.RFC3339))
 	if len(b.volumeKeys) > 0 {
 		// and adding volume mount entries
 		c.VolumeMounts = b.appendVolumeMounts(c.VolumeMounts)
