90b36dd4f7e4e2c8db9242344d0618514e836cfc
Predrag Knezevic <pknezevi@redhat.com>
2020-01-24 20:39:17 +0100

    build image with proper manifests folder
    Change-Id: I06e8de0e28142ce7b67a4d4f6b5bc16d3949d39f

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -6,11 +6,13 @@ source $GENERIC_CHECK_PATCH_PATH || true
 
 # override generic function so that we can use our versioning schemas
 ci_get_xyz_version() {
-    echo "0 0 23"
+    echo "0 0 24"
 }
 
 for container in $(find distgit/containers -mindepth 1 -maxdepth 1 -type d); do
   cat automation/labels >> "$container/Dockerfile.in"
+  cp automation/distgit-gitignore "$container/.gitignore"
 done
+
 
 ci_main "$0"
