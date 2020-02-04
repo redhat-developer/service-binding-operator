7eb24400932b507afbb062a70d1f0066515457d8
Predrag Knezevic <pknezevi@redhat.com>
2020-02-04 15:31:09 +0100

    fixed CSV generation so it could pass CVP validation rules
    Change-Id: I9579ad544c1125ecc94c7512d9cba50e5b92d8d0

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -6,7 +6,7 @@ source $GENERIC_CHECK_PATCH_PATH || true
 
 # override generic function so that we can use our versioning schemas
 ci_get_xyz_version() {
-    echo "0 0 24"
+    echo "0 0 25"
 }
 
 for container in $(find distgit/containers -mindepth 1 -maxdepth 1 -type d); do
