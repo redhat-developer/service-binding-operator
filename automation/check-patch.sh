5943f6aa4286d50e1555ddb81e41fd4b519269f6
Predrag Knezevic <pknezevi@redhat.com>
2020-02-13 16:55:13 +0100

    added logo
    preparing for release
    
    Change-Id: Ibbffad1a78d58636bb5d9c33b882db68dc06a297

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -6,7 +6,7 @@ source $GENERIC_CHECK_PATCH_PATH || true
 
 # override generic function so that we can use our versioning schemas
 ci_get_xyz_version() {
-    echo "0 0 25"
+    echo "0 1 0"
 }
 
 ci_get_branch() {
