58ea511ea0dabc67c2d05cff8ed422807bf278c9
Baiju Muthukadan <baiju.m.mail@gmail.com>
2020-03-09 18:58:14 +0530

    Validate SBO releases (#370)
    After making SBO release through operatorhub.io, validate these releases.
    Ref. https://issues.redhat.com/browse/APPSVC-470

---
+++ hack/validate-release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+set -u
+
+SUBSCRIPTION="./test/operator-hub/subscription2.yaml"
+INSTALL_PLAN_PRIOR=service-binding-operator.v${BUNDLE_VERSION}
+
+# Subscribing to the operator
+kubectl apply -f ${SUBSCRIPTION}
+RUNNING_STATUS=`kubectl get pods -n openshift-marketplace | grep "community-operators" | awk '{print $3}'`
+if [[ ${RUNNING_STATUS} = "Running" ]] ; then
+	echo "Operator marketplace pod is running"
+fi
+./hack/check-crds.sh
+./hack/check-csvs.sh
+INSTALL_PLAN_PRIOR=service-binding-operator.v${BUNDLE_VERSION}
+VERSION_NUMBER=`kubectl get csvs  -n=default -o jsonpath='{.items[*].spec.version}'`
+if [ "${VERSION_NUMBER}" = "${BUNDLE_VERSION}" ] ; then
+    echo -e "OLM Bundle Version validation succeeded \n ";
+	kubectl get csvs -n=default -o jsonpath='{.items[*].metadata.annotations.alm-examples}' | cut -d "[" -f 2 | cut -d "]" -f 1 > output.json;
+	kubectl apply -n=default -f ./output.json;
+	if [ $? = 0 ] ; then
+		echo "CSV alm example validation succeeded"
+	fi
+	if [ `kubectl get installplans -n=openshift-operators -o jsonpath='{.items[*].status.phase}'` == "Complete" ] ; then
+		INSTALL_PLAN=`kubectl get installplans -n=openshift-operators -o jsonpath='{.items[*].spec.clusterServiceVersionNames[0]}'`
+		if [ "${INSTALL_PLAN_PRIOR}" = "${INSTALL_PLAN}" ] ; then
+			echo "Install Plan validation succeeded. OLM Bundle Validation succeeded"
+		fi
+	fi
+	exit 0
+else
+	echo -e "OLM Bundle validation failed \n"
+	echo "Version number: ${VERSION_NUMBER} \nBuild version: ${BUNDLE_VERSION}"
+	exit 1
+fi
