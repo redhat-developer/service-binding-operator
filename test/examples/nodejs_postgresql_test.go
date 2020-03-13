3d34ee6e4c351dad493e81d7c0183c13be54cb8a
Shobith <shobith@localhost.localdomain>
2020-03-13 14:51:54 +0530

    upstream repo with latest changes from pavel

---
+++ test/examples/nodejs_postgresql_test.go
@@ -0,0 +1,81 @@
+package examples_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/redhat-developer/service-binding-operator/test/examples/util"
+	"github.com/stretchr/testify/require"
+)
+
+var exampleName = "nodejs_postgresql"
+var ns = "openshift-operators"
+var ipName, ipStatus, podName, podStatus string
+
+var clusterAvailable = false
+
+//TestSetExampleDir tests that the corrent example directory was set as a working directory for running the commands.
+func TestSetExampleDir(t *testing.T) {
+	examplePath := fmt.Sprintf("%s/%s", util.GetExamplesDir(), exampleName)
+	util.SetDir(examplePath)
+	res := strings.TrimSpace(util.Run("pwd").Stdout())
+	require.Equal(t, examplePath, res)
+}
+
+//Logs the oc status
+func TestGetOCStatus(t *testing.T) {
+	t.Log("--- Getting OC Status ---")
+	result := util.Run("oc", "status")
+	ocStatus := result.ExitCode
+
+	require.Equal(t, ocStatus, 0, "'oc status' is %d", ocStatus)
+	clusterAvailable = true
+
+	t.Log(" *** Connected to cluster *** ")
+}
+
+//Logs out the output of command make install-service-binding-operator
+func TestMakeInstallServiceBindingOperator(t *testing.T) {
+	//	log.Printf("--- CMD executed is make install-service-binding-operator --- %s \n", util.MustSucceed(
+	//"make", "install-service-binding-operator").Stdout())
+
+	checkClusterAvailable(t)
+
+	t.Log("Installing serivice binding operator into the cluster...")
+	res := util.Run("make", "install-service-binding-operator")
+	//log.Printf("--- CMD executed is make install-service-binding-operator --- %s \n", res)
+	t.Log("--- CMD executed is make install-service-binding-operator --- ")
+	t.Logf("-> Status of install-service-binding-operator is %s", res)
+
+	// with openshift-operators namespace, capture the install plan
+	t.Log("Get install plan name from the cluster...")
+	ipName := util.Run("oc", "get", "ip", "-n", ns)
+	t.Log("--- CMD executed is  oc get ip -n openshift-operators --- ")
+	t.Logf("Install plan-ip name is %s", ipName)
+
+	//oc get ip -n openshift-operators <<Name>> -o jsonpath='{.status.phase}'
+	t.Log(" Fetching the status of install plan ")
+	ipStatus := util.Run("oc", "get", "ip", "-n", ns, "-o", "jsonpath='{.status.phase}'")
+	t.Log("--- CMD executed is  oc get ip -n openshift-operators <<Name>> -o jsonpath='{.status.phase}' --- ")
+	t.Logf("-> Status of Install plan-ip is %s", ipStatus)
+
+	//oc get pods -n openshift-operator
+	t.Log("Fetching the pod name of the running pod")
+	podName := util.Run("oc", "get", "pods", "-n", ns)
+	t.Log("--- CMD executed is  oc get pods -n openshift-operator --- ")
+	t.Logf("-> Pod name is %s", podName)
+
+	//oc get pod <<Name of pod(from step 4)>> -n openshift-operators -o jsonpath='{.status.phase}'
+	t.Log("Fetching the status of running pod")
+	podStatus := util.Run("oc", "get", "pods", "-n", ns)
+	t.Log("--- CMD executed is  oc get pod $pods -n $ns -o jsonpath='{.status.phase}' --- ")
+	t.Logf("Pod status is %s", podStatus)
+
+}
+
+func checkClusterAvailable(t *testing.T) {
+	if !clusterAvailable {
+		t.Skip("Cluster is not available, skipping")
+	}
+}
---
+++ test/examples/util/util.go
@@ -0,0 +1,70 @@
+package util
+
+//This will set the corresponding example directory
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"time"
+
+	"gotest.tools/v3/icmd"
+)
+
+const (
+	//Timeout defines the amount of time we should spend waiting for the resource when condition is true
+	Timeout = 10 * time.Minute
+)
+
+var (
+	workingDirOp       icmd.CmdOp = nil
+	kubeConfig                    = os.Getenv("KUBECONFIG")
+	defaultEnvironment            = []string{fmt.Sprintf("KUBECONFIG=%s", kubeConfig)}
+)
+
+//Run runs a command with timeout
+func Run(cmd ...string) *icmd.Result {
+	currentCmd := icmd.Cmd{
+		Command: cmd,
+		Timeout: Timeout,
+		Env:     defaultEnvironment,
+	}
+
+	if workingDirOp != nil {
+		return icmd.RunCmd(currentCmd, workingDirOp)
+	}
+	return icmd.RunCmd(currentCmd)
+}
+
+/*/ MustSucceed asserts that the command ran with 0 exit code
+func MustSucceed(args ...string) *icmd.Result {
+	return Assert(args...)
+}
+
+// Assert runs a command and verifies exit code (0)
+func Assert(args ...string) *icmd.Result {
+	res := Run(args...)
+	//t := &testsuitAdaptor{}
+	//res.Assert(t, exp)
+	return res
+}*/
+
+//SetDir sets the working directory
+func SetDir(path string) {
+	workingDirOp = icmd.Dir(path)
+}
+
+//SetKubeConfig sets the KUBECONFIG to the cluster
+func SetKubeConfig(kc string) {
+	kubeConfig = kc
+}
+
+//GetExamplesDir returns a root path to the examples
+func GetExamplesDir() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get the working dir.")
+	}
+	return path.Clean(fmt.Sprintf("%s/../../examples", wd))
+}
