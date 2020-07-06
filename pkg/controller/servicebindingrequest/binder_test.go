9095df5b1115e03467f8fb8bfbc2ca1b04f3b55e
Avni Sharma <16avnisharma@gmail.com>
2020-07-06 15:13:58 +0530

    Add unit test

--- pkg/controller/servicebindingrequest/binder_test.go
+++ pkg/controller/servicebindingrequest/binder_test.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -55,6 +55,9 @@ func TestBinderNew(t *testing.T) {
 		deploymentsGVR,
 		map[string]string{},
 	)
+
+	f.AddMockedUnstructuredSecret(name)
+	fakeDynClient := f.FakeDynClient()
 
 	t.Run("search-using-resourceref", func(t *testing.T) {
 		binderForSBRWithResourceRef := newBinder(
@@ -169,10 +172,14 @@ func TestBinderNew(t *testing.T) {
 		envVar := getEnvVar(c.Env, changeTriggerEnv)
 		require.NotNil(t, envVar)
 		require.NotEmpty(t, envVar.Value)
-
-		parsedTime, err := time.Parse(time.RFC3339, envVar.Value)
 		require.NoError(t, err)
-		require.True(t, parsedTime.Before(time.Now()))
+
+		uSecret, err := fakeDynClient.Resource(secretsGVR).Get(name, metav1.GetOptions{})
+		require.NoError(t, err)
+		s := corev1.Secret{}
+		err = runtime.DefaultUnstructuredConverter.FromUnstructured(uSecret.Object, &s)
+		require.NoError(t, err)
+		require.Equal(t, s.ObjectMeta.ResourceVersion, envVar.Value)
 
 	})
 
