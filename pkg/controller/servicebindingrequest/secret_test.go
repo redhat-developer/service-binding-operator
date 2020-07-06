e4c906ea0c8479a7093a422c4994cfc34b36d82c
Dhriti Shikhar <dhriti.shikhar.rokz@gmail.com>
2020-07-06 15:32:13 +0530

    Add owner reference to secret (#524)
    * add owner reference to secret
    
    * add tests
    
    * remove import
    
    * Code corrections
    
    [1] Remove copy of passed reference
    [2] Rename parameter from secretOwnerReference to ownerReference
    
    * Code refactoring
    
    [1] Creates `AsOwnerReference()` method on SBR
    [2] Removes secret wrapper function commit()
    [3] Update the tests around secret accordingly
    
    * remove duplicate import
    
    * remove secret deletion method
    
    * Set controller value in secret test
    
    * corrects test

--- pkg/controller/servicebindingrequest/secret_test.go
+++ pkg/controller/servicebindingrequest/secret_test.go
@@ -4,14 +4,26 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"github.com/redhat-developer/service-binding-operator/test/mocks"
 )
 
+var ownerRefController bool = true
+var secretOwnerReference = v1.OwnerReference{
+	Name:       "binding-request",
+	UID:        "c77ca1ae-72d0-4fdd-809f-58fdd37facf3",
+	Kind:       "ServiceBindingRequest",
+	APIVersion: "apps.openshift.io/v1alpha1",
+	Controller: &ownerRefController,
+}
+
 func assertSecretNamespacedName(t *testing.T, u *unstructured.Unstructured, ns, name string) {
 	assert.Equal(t, ns, u.GetNamespace())
 	assert.Equal(t, name, u.GetName())
+	ownerReference := u.GetOwnerReferences()
+	assert.Equal(t, secretOwnerReference, ownerReference[0])
 }
 
 func TestSecretNew(t *testing.T) {
@@ -29,18 +41,7 @@ func TestSecretNew(t *testing.T) {
 	)
 
 	t.Run("createOrUpdate", func(t *testing.T) {
-		u, err := s.createOrUpdate(data)
-		assert.NoError(t, err)
-		assertSecretNamespacedName(t, u, ns, name)
-	})
-
-	t.Run("Delete", func(t *testing.T) {
-		err := s.delete()
-		assert.NoError(t, err)
-	})
-
-	t.Run("Commit", func(t *testing.T) {
-		u, err := s.commit(data)
+		u, err := s.createOrUpdate(data, secretOwnerReference)
 		assert.NoError(t, err)
 		assertSecretNamespacedName(t, u, ns, name)
 	})
