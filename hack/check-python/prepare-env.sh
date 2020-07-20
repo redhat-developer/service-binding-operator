da54bdb6057a94cae23095fdf6c1ac5370918036
Pavel Macík <pavel.macik@gmail.com>
2020-07-20 10:07:38 +0200

    [acceptance-tests] Unify python's venv.

--- hack/check-python/prepare-env.sh
+++ hack/check-python/prepare-env.sh
@@ -1,10 +1,11 @@
-
-directories=${directories:-"./test/acceptance/features"}
+directories=${directories:-"test/acceptance/features"}
 pass=0
 fail=0
 
+export PYTHON_VENV_DIR=${PYTHON_VENV_DIR:-venv}
+
 function prepare_venv() {
-    python3 -m venv venv && source venv/bin/activate
+    python3 -m venv "$PYTHON_VENV_DIR" && source "$PYTHON_VENV_DIR/bin/activate"
     for req in $(find . -name 'requirements.txt'); do
         python3 "$(which pip3)" install -q -r $req;
     done
