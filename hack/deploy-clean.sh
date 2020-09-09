e4972e32c306b90a5cd8f3a26f6d977f57c74c35
Predrag Knezevic <pknezevi@redhat.com>
2020-09-09 19:32:03 +0200

    e2e-cleanup and deploy-cleanup make rules should not fail if testname or some resource does not exist
    * removal of namespace moved to hack/e2e-cleanup.sh
    * if namespace does not exit, do not try to remove it nor to remove any finalizers
    * if CRD is not available, then there is no SBRs that could be tweaked
    * deploy-cleanup.sh should not fail if resource does not exist
    
    Co-authored-by: Pavel Macík <pavel.macik@gmail.com>

--- hack/deploy-clean.sh
+++ hack/deploy-clean.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 
 HACK_DIR=${HACK_DIR:-$(dirname $0)}
 
@@ -6,9 +6,14 @@ HACK_DIR=${HACK_DIR:-$(dirname $0)}
 $HACK_DIR/remove-sbr-finalizers.sh
 
 # Delete deployed resources
-kubectl delete -f deploy/crds/apps_v1alpha1_servicebindingrequest_cr.yaml
-kubectl delete -f deploy/crds/apps.openshift.io_servicebindingrequests_crd.yaml
-kubectl delete -f deploy/operator.yaml
-kubectl delete -f deploy/role_binding.yaml
-kubectl delete -f deploy/role.yaml
-kubectl delete -f deploy/service_account.yaml
+RES_FILES=(
+        crds/apps.openshift.io_servicebindingrequests_crd.yaml
+        operator.yaml
+        role_binding.yaml
+        role.yaml
+        service_account.yaml
+)
+
+for rf in ${RES_FILES[@]} ; do
+    kubectl delete -f "deploy/$rf" --ignore-not-found
+done
--- hack/e2e-cleanup.sh
+++ hack/e2e-cleanup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -xe
 
+[ ! -z "$TEST_NAMESPACE" ] && [ -z "$(kubectl get ns "$TEST_NAMESPACE" -o jsonpath="{.metadata.name}" --ignore-not-found)" ] && exit 0
+
 HACK_DIR=${HACK_DIR:-$(dirname $0)}
 
 # Remove SBR finalizers
 NAMESPACE=$TEST_NAMESPACE $HACK_DIR/remove-sbr-finalizers.sh
+
+[ ! -z "$TEST_NAMESPACE" ] && kubectl delete namespace ${TEST_NAMESPACE} --ignore-not-found --timeout=45s --wait
