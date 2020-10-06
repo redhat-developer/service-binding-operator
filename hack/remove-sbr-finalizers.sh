f179d71ce5f7a6b4e147b8223fdbd8763e64d48d
Pavel Macík <pavel.macik@gmail.com>
2020-10-06 13:02:42 +0200

    Fix hack/remove-sbr-finalizers.sh for new api (#713)

--- hack/remove-sbr-finalizers.sh
+++ hack/remove-sbr-finalizers.sh
@@ -7,11 +7,12 @@ else
 fi
 
 # Remove SBR finalizers if CRD exists
-[ -z "$(kubectl get -f deploy/crds/*crd.yaml -o jsonpath="{.metadata.name}" --ignore-not-found)" ] && exit 0
+CRD_NAME=$(kubectl get -f deploy/crds/*crd.yaml -o jsonpath="{.metadata.name}" --ignore-not-found)
+[ -z $CRD_NAME ] && exit 0
 
-SBRS=($(kubectl get sbrs $USE_NS -o jsonpath="{.items[*].metadata.name}"))
-SBR_NS=($(kubectl get sbrs $USE_NS -o jsonpath="{.items[*].metadata.namespace}"))
+SBRS=($(kubectl get $CRD_NAME $USE_NS -o jsonpath="{.items[*].metadata.name}"))
+SBR_NS=($(kubectl get $CRD_NAME $USE_NS -o jsonpath="{.items[*].metadata.namespace}"))
 
 for i in "${!SBRS[@]}"; do
-    kubectl patch sbr/${SBRS[$i]} -p '{"metadata":{"finalizers":[]}}' --type=merge --namespace=${SBR_NS[$i]};
+    kubectl patch $CRD_NAME/${SBRS[$i]} -p '{"metadata":{"finalizers":[]}}' --type=merge --namespace=${SBR_NS[$i]};
 done
