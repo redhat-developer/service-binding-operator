4cb5e705a8601b57bc9f8b4ecccc14fd704ebe28
Predrag Knezevic <pknezevi@redhat.com>
2020-12-09 09:48:41 +0100

    Add `prepare-operatorhub-pr` target (#794)
    The target extracts files from published bundle image (default: for the current HEAD commit)
    and place them into `out/operatorhub-pr-files/service-binding-operator` folder so that they
    could be easily copied into local clone of `github.com/operator-framework/community-operators`,
    creating PR afterwards.

---
+++ hack/prepare-operatorhub-pr.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S bash -e
+
+# Required CLIs
+# - https://github.com/opencontainers/umoci
+# - https://github.com/containers/skopeo
+
+# Usage:
+# prepare-operatorhu-pr.sh <version> <bundle-image-ref>
+
+TMP_OCI_PATH=$(mktemp -d out/sbo-bundle-oci.XXX)
+
+skopeo copy docker://$2 oci:$TMP_OCI_PATH:bundle
+umoci unpack --image $TMP_OCI_PATH:bundle --rootless ${TMP_OCI_PATH}-unpacked
+rm -rf out/operatorhub-pr-files
+mkdir out/operatorhub-pr-files/service-binding-operator -p
+mv ${TMP_OCI_PATH}-unpacked/rootfs out/operatorhub-pr-files/service-binding-operator/$1
+
+cat <<EOD
+Done.
+
+Now you can copy the content of out/operatorhub-pr-files/service-binding-operator
+into 'upstream-community-operators/service-binding-operator' folder of your local clone of
+
+  github.com/operator-framework/community-operators
+
+and submit a pull request.
+EOD
