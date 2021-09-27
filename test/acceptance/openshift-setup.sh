bda0854b0b12371a6025e2d4ad522d2b95f11456
Pavel Macík <pavel.macik@gmail.com>
2021-09-27 14:21:11 +0200

    [acceptance-tests] Use patch instead of apply for IDP setup (#1033)
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- test/acceptance/openshift-setup.sh
+++ test/acceptance/openshift-setup.sh
@@ -10,22 +10,7 @@ USER_TOKEN=acceptance-tests-dev
 oc get secret $HTPASSWD_SECRET -n openshift-config >/dev/null 2>&1 \
   || oc create secret generic $HTPASSWD_SECRET --from-file=htpasswd=$HTPASSWD_FILE -n openshift-config
 
-oc apply -f - <<EOF
-apiVersion: config.openshift.io/v1
-kind: OAuth
-metadata:
-  name: cluster
-spec:
-  identityProviders:
-  - name: acceptance-tests
-    challenge: true
-    login: true
-    mappingMethod: claim
-    type: HTPasswd
-    htpasswd:
-      fileData:
-        name: $HTPASSWD_SECRET
-EOF
+oc patch oauth cluster --type merge -p '{"spec":{"identityProviders":[{"htpasswd":{"fileData":{"name":"'$HTPASSWD_SECRET'"}},"mappingMethod":"claim","name":"acceptance-tests","challenge":"true","login":true,"type":"HTPasswd"}]}}'
 
 CURRENT_CTX=$(oc config current-context)
 
