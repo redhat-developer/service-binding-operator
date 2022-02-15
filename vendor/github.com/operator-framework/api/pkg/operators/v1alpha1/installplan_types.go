c7249f954474cc4f8f0f70168102f7e7e1a2369b
Baiju Muthukadan <baiju.m.mail@gmail.com>
2022-02-15 11:50:15 +0530

    Update operator-sdk version to v1.17.0
    Ref.
    https://sdk.operatorframework.io/docs/upgrading-sdk-version/v1.17.0/
    
    Signed-off-by: Baiju Muthukadan <baiju.m.mail@gmail.com>

--- vendor/github.com/operator-framework/api/pkg/operators/v1alpha1/installplan_types.go
+++ vendor/github.com/operator-framework/api/pkg/operators/v1alpha1/installplan_types.go
@@ -14,6 +14,7 @@ const (
 )
 
 // Approval is the user approval policy for an InstallPlan.
+// It must be one of "Automatic" or "Manual".
 type Approval string
 
 const (
@@ -70,6 +71,7 @@ const (
 	StepStatusNotPresent          StepStatus = "NotPresent"
 	StepStatusPresent             StepStatus = "Present"
 	StepStatusCreated             StepStatus = "Created"
+	StepStatusNotCreated          StepStatus = "NotCreated"
 	StepStatusWaitingForAPI       StepStatus = "WaitingForApi"
 	StepStatusUnsupportedResource StepStatus = "UnsupportedResource"
 )
@@ -93,6 +95,17 @@ type InstallPlanStatus struct {
 	// AttenuatedServiceAccountRef references the service account that is used
 	// to do scoped operator install.
 	AttenuatedServiceAccountRef *corev1.ObjectReference `json:"attenuatedServiceAccountRef,omitempty"`
+
+	// StartTime is the time when the controller began applying
+	// the resources listed in the plan to the cluster.
+	// +optional
+	StartTime *metav1.Time `json:"startTime,omitempty"`
+
+	// Message is a human-readable message containing detailed
+	// information that may be important to understanding why the
+	// plan has its current status.
+	// +optional
+	Message string `json:"message,omitempty"`
 }
 
 // InstallPlanCondition represents the overall status of the execution of
@@ -215,6 +228,7 @@ func ConditionMet(cond InstallPlanConditionType, now *metav1.Time) InstallPlanCo
 type Step struct {
 	Resolving string       `json:"resolving"`
 	Resource  StepResource `json:"resource"`
+	Optional  bool         `json:"optional,omitempty"`
 	Status    StepStatus   `json:"status"`
 }
 
@@ -261,6 +275,9 @@ type BundleLookup struct {
 	// Conditions represents the overall state of a BundleLookup.
 	// +optional
 	Conditions []BundleLookupCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+	// The effective properties of the unpacked bundle.
+	// +optional
+	Properties string `json:"properties,omitempty"`
 }
 
 // GetCondition returns the BundleLookupCondition of the given type if it exists in the BundleLookup's Conditions.
