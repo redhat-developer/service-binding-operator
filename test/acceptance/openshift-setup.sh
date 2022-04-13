834b585ed470647410dd32114245cf483c35f615
Pavel Macík <pavel.macik@gmail.com>
2022-04-13 14:04:58 +0200

    Create non-admin user for acceptance tests only if it does not exist
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- test/acceptance/openshift-setup.sh
+++ test/acceptance/openshift-setup.sh
@@ -7,19 +7,24 @@ HTPASSWD_SECRET=acceptance-tests-htpass
 USER=acceptance-tests-dev
 USER_TOKEN=acceptance-tests-dev
 
-oc get secret $HTPASSWD_SECRET -n openshift-config >/dev/null 2>&1 \
-  || oc create secret generic $HTPASSWD_SECRET --from-file=htpasswd=$HTPASSWD_FILE -n openshift-config
-
-oc patch oauth cluster --type merge -p '{"spec":{"identityProviders":[{"htpasswd":{"fileData":{"name":"'$HTPASSWD_SECRET'"}},"mappingMethod":"claim","name":"acceptance-tests","challenge":"true","login":true,"type":"HTPasswd"}]}}'
-
 CURRENT_CTX=$(oc config current-context)
+oc login -u $USER -p $USER_TOKEN --loglevel=10 --insecure-skip-tls-verify=true
 
-retries=50
-until [[ $retries == 0 ]]; do
-  oc login -u $USER -p $USER_TOKEN && break
-  sleep 5
-  retries=$(($retries - 1))
-done
+if [ $? -eq 0 ]; then
+  echo "User $USER is already set up, skipping..."
+else
+  oc get secret $HTPASSWD_SECRET -n openshift-config >/dev/null 2>&1 \
+    || oc create secret generic $HTPASSWD_SECRET --from-file=htpasswd=$HTPASSWD_FILE -n openshift-config
+
+  oc patch oauth cluster --type merge -p '{"spec":{"identityProviders":[{"htpasswd":{"fileData":{"name":"'$HTPASSWD_SECRET'"}},"mappingMethod":"claim","name":"acceptance-tests","challenge":"true","login":true,"type":"HTPasswd"}]}}'
+
+  retries=50
+  until [[ $retries == 0 ]]; do
+    oc login -u $USER -p $USER_TOKEN && break
+    sleep 5
+    retries=$(($retries - 1))
+  done;
+fi
 
 oc config set-credentials $USER --token=$(oc whoami --show-token)
 
