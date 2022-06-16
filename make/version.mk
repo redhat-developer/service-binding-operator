fe624991f0cc2dac6836e9fb4d60b980b1b3b9b1
Pavel Macík <pavel.macik@gmail.com>
2022-06-16 18:12:52 +0200

    Add GH action for security scan.
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- make/version.mk
+++ make/version.mk
@@ -12,3 +12,23 @@ OPERATOR_IMAGE_REF ?= $(OPERATOR_REPO_REF):$(GIT_COMMIT_ID)
 OPERATOR_IMAGE_SHA_REF ?= $(shell $(CONTAINER_RUNTIME) inspect --format='{{index .RepoDigests 0}}' $(OPERATOR_IMAGE_REF) | cut -f 2 -d '@')
 OPERATOR_BUNDLE_IMAGE_REF ?= $(OPERATOR_REPO_REF):bundle-$(VERSION)-$(GIT_COMMIT_ID)
 OPERATOR_INDEX_IMAGE_REF ?= $(OPERATOR_REPO_REF):index
+
+.PHONY: operator-repo-ref
+# Prints operator repo ref
+operator-repo-ref:
+	@echo $(OPERATOR_REPO_REF)
+
+.PHONY: operator-image-ref
+# Prints operator image ref
+operator-image-ref:
+	@echo $(OPERATOR_IMAGE_REF)
+
+.PHONY: operator-bundle-image-ref
+# Prints operator bundle image ref
+operator-bundle-image-ref:
+	@echo $(OPERATOR_BUNDLE_IMAGE_REF)
+
+.PHONY: operator-index-image-ref
+# Prints operator index image ref
+operator-index-image-ref:
+	@echo $(OPERATOR_INDEX_IMAGE_REF)
