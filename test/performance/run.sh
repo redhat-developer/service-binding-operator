c8aee45944ca365962433e07167fa6ca3f01da15
Pavel Macík <pavel.macik@gmail.com>
2022-11-21 15:57:55 +0100

    Create cluster-wide resources only once in performance test
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- test/performance/run.sh
+++ test/performance/run.sh
@@ -12,6 +12,7 @@ METRICS_PID=$!
 
 trap "kill -9 $METRICS_PID" EXIT
 
+echo "Waiting for $WAIT_BEFORE to gather resource utilization before the load is started"
 sleep $WAIT_BEFORE
 
 pushd $WS/toolchain-e2e.git
@@ -23,18 +24,27 @@ DEFAULT_WORKLOADS=${DEFAULT_WORKLOADS:-$USERS_PER_SCENARIO}
 SBO_NAMESPACE=${SBO_NAMESPACE:-openshift-operators}
 SBO_DEPLOYMENT=${SBO_DEPLOYMENT:-service-binding-operator}
 
+echo "Applying cluster-wide resources"
+oc apply -f $WS/user-workloads/cluster-wide/sbo-test.yaml
+
+echo "Running the performance scenario: sb-val"
 yes | go run setup/main.go --template=$WS/user-workloads/valid/sbo-test.with-sbr.user-workloads.yaml --users $USERS_PER_SCENARIO --default $DEFAULT_WORKLOADS --custom $USERS_PER_SCENARIO --operators-limit 0 --workloads $SBO_NAMESPACE:$SBO_DEPLOYMENT --username $USER_NS_PREFIX-sb-val > $OUTPUT_DIR/$USER_NS_PREFIX-sb-val.log
 
+echo "Running the performance scenario: nosb-val"
 yes | go run setup/main.go --template=$WS/user-workloads/valid/sbo-test.without-sbr.user-workloads.yaml --users $USERS_PER_SCENARIO --default $DEFAULT_WORKLOADS --custom $USERS_PER_SCENARIO --operators-limit 0 --workloads $SBO_NAMESPACE:$SBO_DEPLOYMENT --username $USER_NS_PREFIX-nosb-val > $OUTPUT_DIR/$USER_NS_PREFIX-nosb-val.log
 $WS/deploy-sbr.sh $WS/user-workloads/valid/sbo-test.sbr.yaml $USER_NS_PREFIX-nosb-val > $OUTPUT_DIR/$USER_NS_PREFIX-nosb-val.deploy-sbr.log
 $WS/deploy-sbr.sh $WS/user-workloads/valid/sbo-test.sbr.spec.yaml $USER_NS_PREFIX-nosb-val >> $OUTPUT_DIR/$USER_NS_PREFIX-nosb-val.deploy-sbr.log
 
+echo "Running the performance scenario: sb-inv"
 yes | go run setup/main.go --template=$WS/user-workloads/invalid/sbo-test.with-sbr.user-workloads.yaml --users $USERS_PER_SCENARIO --default $DEFAULT_WORKLOADS --custom $USERS_PER_SCENARIO --operators-limit 0 --workloads $SBO_NAMESPACE:$SBO_DEPLOYMENT --username $USER_NS_PREFIX-sb-inv > $OUTPUT_DIR/$USER_NS_PREFIX-sb-inv.log
 
+echo "Running the performance scenario: nosb-inv"
 yes | go run setup/main.go --template=$WS/user-workloads/invalid/sbo-test.without-sbr.user-workloads.yaml --users $USERS_PER_SCENARIO --default $DEFAULT_WORKLOADS --custom $USERS_PER_SCENARIO --operators-limit 0 --workloads $SBO_NAMESPACE:$SBO_DEPLOYMENT --username $USER_NS_PREFIX-nosb-inv > $OUTPUT_DIR/$USER_NS_PREFIX-nosb-inv.log
 $WS/deploy-sbr.sh $WS/user-workloads/invalid/sbo-test.sbr.yaml $USER_NS_PREFIX-nosb-inv > $OUTPUT_DIR/$USER_NS_PREFIX-nosb-inv.deploy-sbr.log
 $WS/deploy-sbr.sh $WS/user-workloads/invalid/sbo-test.sbr.spec.yaml $USER_NS_PREFIX-nosb-inv >> $OUTPUT_DIR/$USER_NS_PREFIX-nosb-inv.deploy-sbr.log
 
+echo "Running the performance scenario: sb-inc"
 yes | go run setup/main.go --template=$WS/user-workloads/incomplete/sbo-test.with-sbr.user-workloads.yaml --users $USERS_PER_SCENARIO --default $DEFAULT_WORKLOADS --custom $USERS_PER_SCENARIO --operators-limit 0 --workloads $SBO_NAMESPACE:$SBO_DEPLOYMENT --username $USER_NS_PREFIX-sb-inc > $OUTPUT_DIR/$USER_NS_PREFIX-sb-inc.log
 
+echo "Waiting for $WAIT_AFTER to gather resource utilization after the load is done"
 sleep $WAIT_AFTER
