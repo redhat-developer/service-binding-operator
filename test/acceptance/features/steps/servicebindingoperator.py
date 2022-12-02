17ab9ed4d1465e814acbc336f717a7aa9002c031
Pavel Macík <pavel.macik@gmail.com>
2022-12-02 10:21:10 +0100

    Fix SBO checks for OpenShift 4.9
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- test/acceptance/features/steps/servicebindingoperator.py
+++ test/acceptance/features/steps/servicebindingoperator.py
@@ -38,15 +38,14 @@ class Servicebindingoperator():
             return False
 
         if csv_version is not None:
-            cmd = f"{ctx.cli} wait --for=jsonpath='{{.metadata.labels.olm\\.owner}}'={csv_version} \
-                deployment/service-binding-operator -n {sbo_namespace} --timeout=300s"
-            output, code = self.cmd.run(cmd)
+            cmd = f"{ctx.cli} get deployment/service-binding-operator -o jsonpath='{{.metadata.labels.olm\\.owner}}' -n {sbo_namespace}"
+            (output, code) = polling2.poll(target=lambda: tuple(self.cmd.run(cmd)),
+                                           check_success=lambda o: o[1] == 0 and o[0] == csv_version, step=5, timeout=300, ignore_exceptions=(ValueError,))
             assert code == 0, f"Non-zero return code while trying to check SBO deployment is owned by {csv_version} CSV: {output}"
 
-            cmd = f"{ctx.cli} wait --for=jsonpath='{{.metadata.ownerReferences[0].name}}'={csv_version} \
-                secrets/service-binding-operator-service-cert -n {sbo_namespace}"
-            output, code = polling2.poll(target=lambda: tuple(self.cmd.run(cmd)),
-                                         check_success=lambda o: o[1] == 0, step=5, timeout=300, ignore_exceptions=(ValueError,))
+            cmd = f"{ctx.cli} get secrets/service-binding-operator-service-cert -o jsonpath='{{.metadata.ownerReferences[0].name}}' -n {sbo_namespace}"
+            (output, code) = polling2.poll(target=lambda: tuple(self.cmd.run(cmd)),
+                                           check_success=lambda o: o[1] == 0 and o[0] == csv_version, step=5, timeout=300, ignore_exceptions=(ValueError,))
             assert code == 0, f"Non-zero return code while trying to check SBO cert secret is owned by {csv_version} CSV: {output}"
 
         output, code = self.cmd.run(f"{ctx.cli} rollout status -w deployment/service-binding-operator -n {sbo_namespace}")
